c8bd35ae14e8a9ba5d88f31b23f8c1237c347064
Paul Varache <perso@paulvarache.ninja>
2019-01-22 12:36:42 +0000

    Tests, lint, Jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,56 +1,58 @@
 #!groovy
 
+@Library('kanolib') _
+
 pipeline {
     agent {
-        label 'win-test'
+        label 'ubuntu_18.04_with_docker'
     }
-
+    post {
+        always {
+            junit allowEmptyResults: true, testResults: 'test-results.xml'
+            step([$class: 'CheckStylePublisher', pattern: 'eslint.xml'])
+        }
+        regression {
+            notify_culprits currentBuild.result
+        }
+    }
     stages {
-        // pulls down locally the sources for the component
         stage('checkout') {
             steps {
                 checkout scm
             }
         }
-
-        // Install the bower dependencies of the component
         stage('install dependencies') {
             steps {
                 script {
-                    sh "bower --version || npm i -g bower"
-                    sh "polymer --version || npm i -g polymer-cli"
-                    sh "npm install -g https://github.com/marcelmeulemans/wct-junit-reporter.git"
-                    sh "bower i"
+                    docker.image('node:8-alpine').inside {
+                        sh "yarn"
+                    }
                 }
             }
         }
-
-        // Lints, and tests the component
+        stage('lint') {
+            steps {
+                script {
+                    docker.image('node:8-alpine').inside {
+                        sh "yarn lint-ci"
+                    }
+                }
+            }
+        }
         stage('test') {
             steps {
                 script {
-                    sh "polymer lint"
-                    sh "polymer test --local chrome"
-                    junit allowEmptyResults: true, testResults: 'wct.xml'
-                }
-            }
-        }
-
-        stage('documentation') {
-            steps {
-                script {
-                    if (env.BRANCH_NAME == 'master') {
-                        build job: 'Kano/components-doc/master', parameters: [
-                            text(name: 'repoUrl', value: 'https://github.com/KanoComponents/kwc-pagination'),
-                            text(name: 'componentName', value: 'kwc-pagination')
-                        ], wait: false
+                    // Use puppeteer enabled docker image
+                    docker.image('kanocomputing/puppeteer').inside('--cap-add=SYS_ADMIN') {
+                        // Run the Unit test
+                        sh "yarn test-ci"
                     }
                 }
             }
         }
     }
-
     options {
+        // Only keep a fixed amount of builds
         buildDiscarder(logRotator(numToKeepStr: '20'))
     }
 }
--- kwc-pagination.js
+++ kwc-pagination.js
@@ -1,268 +1,283 @@
-import { PolymerElement } from '../../@polymer/polymer/polymer-element.js';
-import { GestureEventListeners } from '../../@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import '../../@polymer/iron-flex-layout/iron-flex-layout.js';
-import '../../@kano/kwc-button/kwc-soft-button.js';
-import { html } from '../../@polymer/polymer/lib/utils/html-tag.js';
-class KWCPagination extends GestureEventListeners(PolymerElement) {
-  static get template() {
-    return html`
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import { button, soft } from '@kano/styles/button.js';
+// Missing from button.js
+import '@kano/styles/color.js';
+
+class KWCPagination extends PolymerElement {
+    static get template() {
+        return html`
+        ${button}
+        ${soft}
         <style>
             :host {
                 display: block;
+            }
+            .btn {
+                min-width: initial !important;
             }
             *[hidden] {
                 display: none;
             }
             @media all and (max-width: 680px) {
                 :host {
-                    @apply --layout-horizontal;
-                    @apply --layout-wrap;
-                    @apply --layout-center;
-                    @apply --layout-center-justified;
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: wrap;
+                    align-items: center;
+                    justify-content: center;
                 }
                 .item {
                     display: none;
                 }
-                kwc-soft-button ~ kwc-soft-button {
+                .btn.soft ~ .btn.soft {
                     margin-left: 8px;
                 }
             }
         </style>
 
         <template is="dom-if" if="[[jumpControls]]">
-            <kwc-soft-button class="first" on-tap="_goToFirstPage" disabled\$="[[_isFirstPage(currentPage, totalPages)]]">
+            <button class="btn soft first" on-click="_goToFirstPage" disabled$="[[_isFirstPage(currentPage, totalPages)]]">
                 <slot name="firstPage">&lt;&lt;</slot>
-            </kwc-soft-button>
+            </button>
         </template>
 
         <template is="dom-if" if="[[paginationControls]]">
-            <kwc-soft-button class="previous" on-tap="_previousPage" disabled\$="[[_isFirstPage(currentPage, totalPages)]]">
+            <button class="btn soft previous" on-click="_previousPage" disabled$="[[_isFirstPage(currentPage, totalPages)]]">
                 <slot name="previousPage">PREV</slot>
-            </kwc-soft-button>
+            </button>
         </template>
 
         <template is="dom-repeat" items="[[_pages]]">
-            <kwc-soft-button class="item" active="[[_isActive(item, currentPage)]]" on-tap="_goToPage">
+            <button class="btn soft item" active$="[[_isActive(item, currentPage)]]" on-click="_goToPage">
                 [[item]]
-            </kwc-soft-button>
+            </button>
         </template>
 
         <template is="dom-if" if="[[paginationControls]]">
-            <kwc-soft-button class="next" on-tap="_nextPage" disabled\$="[[_isLastPage(currentPage, totalPages)]]">
+            <button class="btn soft next" on-click="_nextPage" disabled$="[[_isLastPage(currentPage, totalPages)]]">
                 <slot name="nextPage">NEXT</slot>
-            </kwc-soft-button>
+            </button>
         </template>
 
         <template is="dom-if" if="[[jumpControls]]">
-            <kwc-soft-button class="last" on-tap="_goToLastPage" disabled\$="[[_isLastPage(currentPage, totalPages)]]">
+            <button class="btn soft last" on-click="_goToLastPage" disabled$="[[_isLastPage(currentPage, totalPages)]]">
                 <slot name="lastPage">&gt;&gt;</slot>
-            </kwc-soft-button>
+            </button>
         </template>
 `;
-  }
+    }
 
-  static get is () { return 'kwc-pagination'; }
-  static get properties () {
-      return {
-          /**
-           * Set what is the number to start counting from for page
-           * labels.
-           */
-          startsOn: {
-              type: Number,
-              value: 1
-          },
-          /**
-           * Total amount of pages to paginate.
-           * @type {Number}
-           */
-          totalPages: {
-              type: Number,
-              value: 1
-          },
-          /**
-           * Which page is currently selected.
-           * @type {Number}
-           */
-          currentPage: {
-              type: Number,
-              value: 1
-          },
-          /**
-           * How many pages to display between the control buttons.
-           * @type {Number}
-           */
-          range: {
-              type: Number,
-              value: 3
-          },
-          /**
-           * Flags if pagination should display controls for "next"
-           * and "previous" pages.
-           * @type {Boolean}
-           */
-          paginationControls: {
-              type: Boolean,
-              default: false
-          },
-          /**
-           * Flags if pagination should display controls for "jump to
-           * first" and "jump to last" pages.
-           * @type {Boolean}
-           */
-          jumpControls: {
-              type: Boolean,
-              default: false
-          },
-          /**
-           * Array of page indexes to be displayed, calculated based
-           * on the `totalPages`, `range` and the `currentPage`.
-           * @type {Array}
-           */
-          _pages: {
-              type: Array,
-              computed: '_computePages(currentPage, totalPages, range)'
-          },
+    static get is() { return 'kwc-pagination'; }
 
-      }
-  }
-  connectedCallback () {
-      super.connectedCallback();
-  }
+    static get properties() {
+        return {
+            /**
+             * Set what is the number to start counting from for page
+             * labels.
+             */
+            startsOn: {
+                type: Number,
+                value: 1,
+            },
+            /**
+             * Total amount of pages to paginate.
+             * @type {Number}
+             */
+            totalPages: {
+                type: Number,
+                value: 1,
+            },
+            /**
+             * Which page is currently selected.
+             * @type {Number}
+             */
+            currentPage: {
+                type: Number,
+                value: 1,
+            },
+            /**
+             * How many pages to display between the control buttons.
+             * @type {Number}
+             */
+            range: {
+                type: Number,
+                value: 3,
+            },
+            /**
+             * Flags if pagination should display controls for "next"
+             * and "previous" pages.
+             * @type {Boolean}
+             */
+            paginationControls: {
+                type: Boolean,
+                default: false,
+            },
+            /**
+             * Flags if pagination should display controls for "jump to
+             * first" and "jump to last" pages.
+             * @type {Boolean}
+             */
+            jumpControls: {
+                type: Boolean,
+                default: false,
+            },
+            /**
+             * Array of page indexes to be displayed, calculated based
+             * on the `totalPages`, `range` and the `currentPage`.
+             * @type {Array}
+             */
+            _pages: {
+                type: Array,
+                computed: '_computePages(currentPage, totalPages, range)',
+            },
 
-  _computePages (currentPage, totalPages, range) {
-      totalPages = (totalPages || 1);
-      let pages = [],
-          startsOn = this.startsOn;
+        };
+    }
 
-      if (totalPages <= range) {
-          /**
-           * The amount of pages is smaller than the range:
-           * add as many pages as there are
-           */
-          for (let i = startsOn; i < totalPages+startsOn; i++) {
-              pages.push(i);
-          }
-      } else {
-          /**
-           * The amount of pages is bigger than the range:
-           * only show a number of pages equal to the range
-           *
-           * If current page is not on the last pages
-           */
-          if (currentPage < (startsOn + totalPages - range)) {
-              /**
-               * Current page is in the middle of pagination
-               */
-              let initCount = currentPage - parseInt(range/2);
-              if (initCount < startsOn) {
-                  initCount = startsOn;
-              }
-              for (let i = initCount; i < initCount+range; i++) {
-                  pages.push(i);
-              }
-          } else {
-              /**
-               * Current page is on the final pages
-               */
-              let initCount = startsOn + totalPages - range;
-              for (let i = initCount; i < totalPages+startsOn; i++) {
-                  pages.push(i);
-              }
-          }
-      }
-      return pages;
-  }
-  /**
-   * Triggered to notify an intent to navigate to a page (from the
-   * available pages on the navigation).
-   * @event go-to-page
-   * @param {Object} e Object containing information if the event
-   *     should bubble up (`bubbles`) and the page index intended
-   *    (`detail`).
-   */
-  /**
-   * Dispath event to notify intent to navigate to a specific page
-   * @param {Event} e Event triggered by tapping a page button.
-   */
-  _goToPage (e) {
-      let page = parseInt(e.model.item);
-      this.dispatchEvent(new CustomEvent(
-          'go-to-page', { bubbles: false, detail: page }
-      ));
-  }
-  /**
-   * Dispath event to notify intent to navigate to the next page
-   * @param {Event} e Event triggered by tapping a page button.
-   */
-  _nextPage (e) {
-      let page = parseInt(this.currentPage);
-      this.dispatchEvent(new CustomEvent(
-          'go-to-page', { bubbles: false, detail: page + 1 }
-      ));
-  }
-  /**
-   * Dispath event to notify intent to navigate to the previous page
-   * @param {Event} e Event triggered by tapping a page button.
-   */
-  _previousPage (e) {
-      let page = parseInt(this.currentPage);
-      this.dispatchEvent(new CustomEvent(
-          'go-to-page', { bubbles: false, detail: page - 1 }
-      ));
-  }
-  /**
-   * Dispath event to notify intent to navigate to the jump to the
-   * last page.
-   * @param {Event} e Event triggered by tapping a page button.
-   */
-  _goToLastPage () {
-      this.dispatchEvent(new CustomEvent(
-          'go-to-page', { bubbles: false, detail: this.totalPages + this.startsOn - 1 }
-      ));
-  }
-  /**
-   * Dispath event to notify intent to navigate to the jump to the
-   * first page.
-   * @param {Event} e Event triggered by tapping a page button.
-   */
-  _goToFirstPage () {
-      this.dispatchEvent(new CustomEvent(
-          'go-to-page', { bubbles: false, detail: this.startsOn }
-      ));
-  }
-  /**
-   * Calculates if the given page is the currently selected/active
-   * @param {Number} page Page to check if it's selected/active
-   * @return {Boolean}
-   */
-  _isActive (page) {
-      return page == this.currentPage;
-  }
-  /**
-   * Calculates if the current page is the first page.
-   * @return {Boolean}
-   */
-  _isFirstPage () {
-      // If there are pages, check if it's the first
-      if (this.totalPages > 1) {
-          return parseInt(this.currentPage) == this.startsOn;
-      } else {
-          // Return true to disable the "previous" and "first page"
-          // buttons on the navigation
-          return true;
-      }
-  }
-  /**
-   * Calculates if the current page is the last page.
-   * @return {Boolean}
-   */
-  _isLastPage () {
-      if (this.totalPages > 1) {
-          return parseInt(this.currentPage) == this.totalPages + this.startsOn - 1;
-      } else {
-          return true
-      }
-  }
+    connectedCallback() {
+        super.connectedCallback();
+    }
+
+    _computePages(currentPage, totalPages = 1, range) {
+        const pages = [];
+
+
+        const { startsOn } = this;
+
+        if (totalPages <= range) {
+            /**
+             * The amount of pages is smaller than the range:
+             * add as many pages as there are
+             */
+            for (let i = startsOn; i < totalPages + startsOn; i += 1) {
+                pages.push(i);
+            }
+        /**
+         * The amount of pages is bigger than the range:
+         * only show a number of pages equal to the range
+         *
+         * If current page is not on the last pages
+         */
+        } else if (currentPage < (startsOn + totalPages - range)) {
+            /**
+             * Current page is in the middle of pagination
+             */
+            let initCount = currentPage - parseInt(range / 2, 10);
+            if (initCount < startsOn) {
+                initCount = startsOn;
+            }
+            for (let i = initCount; i < initCount + range; i += 1) {
+                pages.push(i);
+            }
+        } else {
+            /**
+             * Current page is on the final pages
+             */
+            const initCount = startsOn + totalPages - range;
+            for (let i = initCount; i < totalPages + startsOn; i += 1) {
+                pages.push(i);
+            }
+        }
+        return pages;
+    }
+
+    /**
+     * Triggered to notify an intent to navigate to a page (from the
+     * available pages on the navigation).
+     * @event go-to-page
+     * @param {Object} e Object containing information if the event
+     *     should bubble up (`bubbles`) and the page index intended
+     *    (`detail`).
+     */
+    /**
+     * Dispath event to notify intent to navigate to a specific page
+     * @param {Event} e Event triggered by tapping a page button.
+     */
+    _goToPage(e) {
+        const page = parseInt(e.model.item, 10);
+        this.dispatchEvent(new CustomEvent(
+            'go-to-page', { bubbles: false, detail: page },
+        ));
+    }
+
+    /**
+     * Dispath event to notify intent to navigate to the next page
+     * @param {Event} e Event triggered by tapping a page button.
+     */
+    _nextPage() {
+        const page = parseInt(this.currentPage, 10);
+        this.dispatchEvent(new CustomEvent(
+            'go-to-page', { bubbles: false, detail: page + 1 },
+        ));
+    }
+
+    /**
+     * Dispath event to notify intent to navigate to the previous page
+     * @param {Event} e Event triggered by tapping a page button.
+     */
+    _previousPage() {
+        const page = parseInt(this.currentPage, 10);
+        this.dispatchEvent(new CustomEvent(
+            'go-to-page', { bubbles: false, detail: page - 1 },
+        ));
+    }
+
+    /**
+     * Dispath event to notify intent to navigate to the jump to the
+     * last page.
+     * @param {Event} e Event triggered by tapping a page button.
+     */
+    _goToLastPage() {
+        this.dispatchEvent(new CustomEvent(
+            'go-to-page', { bubbles: false, detail: this.totalPages + this.startsOn - 1 },
+        ));
+    }
+
+    /**
+     * Dispath event to notify intent to navigate to the jump to the
+     * first page.
+     * @param {Event} e Event triggered by tapping a page button.
+     */
+    _goToFirstPage() {
+        this.dispatchEvent(new CustomEvent(
+            'go-to-page', { bubbles: false, detail: this.startsOn },
+        ));
+    }
+
+    /**
+     * Calculates if the given page is the currently selected/active
+     * @param {Number} page Page to check if it's selected/active
+     * @return {Boolean}
+     */
+    _isActive(page) {
+        return page === this.currentPage;
+    }
+
+    /**
+     * Calculates if the current page is the first page.
+     * @return {Boolean}
+     */
+    _isFirstPage() {
+        // If there are pages, check if it's the first
+        if (this.totalPages > 1) {
+            return parseInt(this.currentPage, 10) === this.startsOn;
+        }
+        // Return true to disable the "previous" and "first page"
+        // buttons on the navigation
+        return true;
+    }
+
+    /**
+     * Calculates if the current page is the last page.
+     * @return {Boolean}
+     */
+    _isLastPage() {
+        if (this.totalPages > 1) {
+            return parseInt(this.currentPage, 10) === this.totalPages + this.startsOn - 1;
+        }
+        return true;
+    }
 }
 window.customElements.define(KWCPagination.is, KWCPagination);
